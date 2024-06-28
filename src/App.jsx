f2e98102b99f73ad0ec42456bfb4c4588a871954
fardin-faisal <fardin00faisal05@gmail.com>
2024-06-28 20:50:18 +0600

    About Page and Contact Page completed!

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from './common/Header';
-import HomePage from './pages/home';
-import CategoryPage from './pages/category';
+import { Routes, Route } from "react-router-dom";
+import Header from "./common/Header";
+import HomePage from "./pages/home";
+import CategoryPage from "./pages/category";
+import AboutPage from "./pages/about/aboutUs";
+import ContactPage from "./pages/contact/contactUs";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/category/:name' element={<CategoryPage />} />
-        <Route path='/about' element={<>About Page</>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/category/:name" element={<CategoryPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </>
   );
